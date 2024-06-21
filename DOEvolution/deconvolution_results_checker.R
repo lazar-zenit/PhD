7883542770461c3b5c6f554bf36f4c5474eec40b
lazar-zenit <62837269+lazar-zenit@users.noreply.github.com>
2024-06-21 18:08:08 +0200

    update deconvolution_results_checker.py

--- DOEvolution/deconvolution_results_checker.R
+++ DOEvolution/deconvolution_results_checker.R
@@ -31,19 +31,30 @@ cat("Value summary stats:\n", value_summary, "\n",
     )
 
 # enter yout bands of interest
-bands_of_interest = c(1079)
+bands_of_interest = c(1079, 1038, 1024)
+bad_bands = c(1144)
 
 # see distribution of values
 library(ggplot2)
 ggplot(filtered_df, aes(x = value)) + 
-  geom_histogram(bins = 20, fill = "blue", alpha = 0.5, color = "black") +
+  geom_histogram(bins = 30, fill = "blue", alpha = 0.5, color = "black") +
   #geom_errorbar(aes(ymin = value - error, ymax = value + error, x = value),
                 #width = 0.2, color = "red") +
-  scale_x_continuous(breaks = seq(900, 1150, by=20)) +
-  geom_vline(x_intercept = bands_of_interest, color = "red", size = 1)
+  scale_x_continuous(breaks = seq(900, 1150, by=10)) +
+  geom_vline(xintercept = bands_of_interest, color = "red", size = 1, linetype = "dashed") +
+  geom_vline(xintercept = bad_bands, color = "black", size = 1, linetype = "dashed") +
   theme_minimal() +
   labs(title = "Distribution of convoluted peak centers",
        x = "value",
-       y = "frequency")
-  
+       y = "frequency") +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1),
+        plot.title = element_text(hjust = 0.5))
 
+# filter values based on wavenumber
+values_w1 = filtered_df %>%
+  filter(grepl("center", param) & between(value, 1079-5, 1079+5))
+View(values_w1)
+
+values_w2 = filtered_df %>%
+  filter(grepl("center", param) & between(value, 1038-5, 1038+5))
+View(values_w2)
