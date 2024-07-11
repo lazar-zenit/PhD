be1e5c6e2a5f9ab0cfe391ee76cc17d9a608cb78
lazar-zenit <62837269+lazar-zenit@users.noreply.github.com>
2024-07-11 17:20:46 +0200

    update

---
+++ SpectralTool/openspecy.R
@@ -0,0 +1,46 @@
+# setup working directory
+setwd("C:/Users/Lenovo/Documents/Programiranje/PhD/SpectralTool/datasets/viminacijum_out")
+
+# load the libraries
+library(OpenSpecy)
+
+
+# IMPORT FILE
+data = read_text("PedjaB01_processed.csv")
+
+# plot the raw data for inspection
+plot(data)
+
+
+
+# baseline correction
+data_1 = subtr_baseline(data,
+               type = "polynomial",
+               degree = 8,
+               raw = TRUE,
+               make_rel = TRUE
+               ) 
+
+plot(data_1)
+
+# smooth
+data_2 = smooth_intens(data_1,
+                       polynomal = 4,
+                       window = 49, 
+                       derivative = 0,
+                       type = "wh",
+                       lambda = 10500,
+                       d = 2
+                       )
+plot(data_2)
+
+
+# smooth
+data_3 = smooth_intens(data_2,
+                       polynomal = 4,
+                       window = 49, 
+                       derivative = 0,
+                       type = "sg",
+                       wavenum_width = 100)
+plot(data_3)
+
