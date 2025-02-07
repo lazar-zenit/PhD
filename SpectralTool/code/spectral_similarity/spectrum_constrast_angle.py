f360e93feae976eee54da00d980164017fb72360
lazar-zenit <62837269+lazar-zenit@users.noreply.github.com>
2025-02-07 14:21:22 +0100

    Cosmetic update

--- SpectralTool/code/spectral_similarity/spectrum_constrast_angle.py
+++ SpectralTool/code/spectral_similarity/spectrum_constrast_angle.py
@@ -48,7 +48,7 @@ def spectral_contrast_angle_matrix(sample_data):
 
     # Handle errors if there is no matrix
     if similarity_matrix is not None:
-        print("Similarity matrix:")
+        print("\nCosine Similarity matrix:")
         print(similarity_matrix)
 
     else:
@@ -59,14 +59,34 @@ def spectral_contrast_angle_matrix(sample_data):
     angle_rad = np.arccos(similarity_matrix)
     angle_deg = np.round((np.degrees(angle_rad)), decimals=2)
 
-    print("\n Spectral Contrast angle (degrees):")
+    print("\n Spectral Contrast Angle (degrees):")
     print(angle_deg)
 
     return angle_deg
 
 
+def spectral_contrast_compare(spectrum_A, spectrum_B):
+    s1 = data[spectrum_A]
+    s2 = data[spectrum_B]
+
+    cos_sim = cosine_similarity([s1], [s2])
+    angle_rad = np.arccos(cos_sim)
+    angle_deg = np.degrees(angle_rad)
+
+    print(f"\nComparison between spectra {spectrum_A} and {spectrum_B}:")
+    print("Cosine Similatity: ", np.round((cos_sim[0][0]), decimals=5))
+    print("Spectral Contrast Angle (degrees)",
+          np.round((angle_deg[0][0]), decimals=2))
+
+    return cos_sim, angle_deg
+
+
 # User input
 FILEPATH = "C:/Users/Lenovo/Documents/Programiranje/PhD/SpectralTool/datasets/central_points_good_mva_transposed.csv"
+
+spectrum_A = "c1"
+spectrum_B = "c2"
+
 
 # Import spectra
 data = import_spectra(FILEPATH)
@@ -74,30 +94,32 @@ data = import_spectra(FILEPATH)
 # Check if data is imported correctly
 if data:
     for sample, intensities in data.items():
-        print(f"Vector shape: {intensities.shape}")
+        print(f"Vector {sample} shape: {intensities.shape}")
 
 else:
     print("Data import failed!")
 
 # Calculate spectral contrast angle matrices
-angle_deg = spectral_contrast_angle_matrix(data)
+angle_deg_matrix = spectral_contrast_angle_matrix(data)
 
+# Compare two spectra
+spectral_contrast_compare(spectrum_A, spectrum_B)
 
 # Plot the results - rough
 plt.figure(figsize=(8, 6))
-plt.imshow(angle_deg, cmap='viridis', origin='upper', extent=[
-           0, angle_deg.shape[1], angle_deg.shape[0], 0])
+plt.imshow(angle_deg_matrix, cmap='viridis', origin='upper', extent=[
+           0, angle_deg_matrix.shape[1], angle_deg_matrix.shape[0], 0])
 plt.colorbar(label='Angle (degrees)')  # Add a colorbar
 plt.title('Spectral Contrast Angle Matrix')
 plt.xlabel('Spectrum Index')
 plt.ylabel('Spectrum Index')
 
 # For larger matrices, annotations can make the plot cluttered
-if angle_deg.shape[0] < 10:  # Adjust condition as needed
-    for i in range(angle_deg.shape[0]):
-        for j in range(angle_deg.shape[1]):
+if angle_deg_matrix.shape[0] < 10:  # Adjust condition as needed
+    for i in range(angle_deg_matrix.shape[0]):
+        for j in range(angle_deg_matrix.shape[1]):
             # 'w' for white text
-            plt.text(j, i, f'{angle_deg[i, j]:.2f}',
+            plt.text(j, i, f'{angle_deg_matrix[i, j]:.2f}',
                      ha='center', va='center', color='w')
 
 plt.tight_layout()  # Adjust layout to prevent labels from overlapping
